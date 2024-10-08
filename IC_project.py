7f1e3b766a822a4e9dbcdeabcdca9bc1d536ddb4
Eric Souza <75805969+EricSouzaa@users.noreply.github.com>
2024-10-07 22:48:12 -0300

    New Update IC_project.py
    - Colorbar Label
    - Yticks removed

--- IC_project.py
+++ IC_project.py
@@ -5,7 +5,7 @@ import pandas as pd
 from matplotlib.colors import LinearSegmentedColormap
 
 
-def delta_met(data: pd.DataFrame, x_label: str, y_label: str, x_limits: float, y_limits: float, file_name: str,
+def delta_met(data: pd.DataFrame, x_label: str, y_label: str, colorbar_label: str, x_limits: float, y_limits: float, file_name: str,
               dpi: int, colors: list, colorbar_limits=(4500, 6000)):
 
     if colors is None:
@@ -36,7 +36,6 @@ def delta_met(data: pd.DataFrame, x_label: str, y_label: str, x_limits: float, y
     ax1.tick_params(axis='both', left=True, top=True, right=True, bottom=True, labelleft=True, labeltop=False,
                     labelright=False, labelbottom=False)
 
-    # ax1.set_xticks(np.linspace(x_limits[0], x_limits[1], 5))
     ax1.tick_params(axis='x', which='minor', top=True, direction='in', size=1, width=0.5)
     ax1.tick_params(axis='y', which='minor', right=True, direction='in', size=1, width=0.5)
     ax1.tick_params(axis='x', which='major', top=True, direction='in', size=2, width=0.5)
@@ -52,7 +51,7 @@ def delta_met(data: pd.DataFrame, x_label: str, y_label: str, x_limits: float, y
     ax2.set_xlim(x_limits)
     ax2.set_ylim(min(x - y) + min(x - y) * 0.2, max(x - y) + max(x - y) * 0.2)
 
-    ax2.set_yticks([np.round(min(x - y), 2), 0, np.round(max(x - y), 2)])
+    ax2.tick_params(axis='y', which='both', left=False, labelleft=False)
     ax2.tick_params(axis='both', labelsize=8)  # Set font size for both x and y axes
 
     ax2.set_xlabel(x_label, fontsize=8)
@@ -63,13 +62,12 @@ def delta_met(data: pd.DataFrame, x_label: str, y_label: str, x_limits: float, y
 
     cax = plt.axes([0.82, 0.33, 0.03, 0.6])
     cbar = plt.colorbar(scatter, cax=cax)
-    cbar.set_label(r'$T_{\rm eff}$', fontsize=10)
+    cbar.set_label(colorbar_label, fontsize=10)
     cbar.ax.tick_params(labelsize=6, size=2)
 
     ax2.tick_params(axis='both', left=True, top=True, right=True, bottom=True, labelleft=True, labeltop=False,
                     labelright=False, labelbottom=True)
 
-    # ax1.set_xticks(np.linspace(x_limits[0], x_limits[1], 5))
     ax2.tick_params(axis='x', which='minor', top=True, direction='in', size=1, width=0.5)
     ax2.tick_params(axis='y', which='minor', right=True, direction='in', size=1, width=0.5)
     ax2.tick_params(axis='x', which='major', top=True, direction='in', size=2, width=0.5)
@@ -86,8 +84,8 @@ np.random.seed(42)
 num_points = 100
 data = {
     'Mg_trends': [{
-        'Mg': np.random.uniform(-0.1, 0.1, num_points),
-        'Mg_H_raw_Syn_NLTE': np.random.uniform(-0.1, 0.1, num_points),
+        'Mg': np.random.uniform(-0.1,0.1, num_points),
+        'Mg_H_raw_Syn_NLTE': np.random.uniform(-0.5,0.5, num_points),
         'Teff_raw_Syn_NLTE': np.random.uniform(4600, 6000, num_points)
     }]
 }
@@ -100,10 +98,11 @@ delta_met(
     data=df,
     x_label='[Mg/H] (This Work)',
     y_label='[Mg/H] (Syntspec DR17 NLTE)',
+    colorbar_label='Teff',
     x_limits=(-0.19, 0.19),
     y_limits=(-0.19, 0.19),
     file_name='figure_delta_Mg_Thiswork_close',
     dpi=500,
-    colors=None,
-    colorbar_limits=(4500, 6000)
+    colors=tailwind_colors,
+    colorbar_limits=(1, 6000)
 )
